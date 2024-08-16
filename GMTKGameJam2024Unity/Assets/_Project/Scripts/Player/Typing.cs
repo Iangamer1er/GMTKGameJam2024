fe8d315a753b72ab1250bee143220a8efef9e3c9
Iangamer1er <Iangamer1er@gmail.com>
2024-08-16 16:56:17 -0400

    chek for words existing in the list done

--- GMTKGameJam2024Unity/Assets/_Project/Scripts/Player/Typing.cs
+++ GMTKGameJam2024Unity/Assets/_Project/Scripts/Player/Typing.cs
@@ -1,13 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Typing : MonoBehaviour
 {
     [SerializeField] FormatedText text;
+    [SerializeField] string testString = "A";
 
     private void Start() {
-        // Debug.Log(text.words[0]);
+        Debug.Log(text.words[50]);
+        Debug.Log(CheckList(text.words, testString));
+    }
+
+    private bool CheckList(List<string> list, string word)
+    {
+        return list.Any(e => e.ToLower().Contains(word.ToLower()));
     }
 
     [ContextMenu("Format")]
